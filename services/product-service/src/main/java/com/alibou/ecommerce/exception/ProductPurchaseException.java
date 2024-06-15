1494e3709032411eb2923b578d65a694924809e3
Nibras Sameeh <nibras.s@aeturnum.com>
2024-06-15 21:04:02 +0530

    fixed exception handling in product-service

--- services/product-service/src/main/java/com/alibou/ecommerce/exception/ProductPurchaseException.java
+++ services/product-service/src/main/java/com/alibou/ecommerce/exception/ProductPurchaseException.java
@@ -3,6 +3,6 @@ package com.alibou.ecommerce.exception;
 public class ProductPurchaseException extends RuntimeException {
 
     public ProductPurchaseException(String s) {
-
+        super(s);
     }
 }
